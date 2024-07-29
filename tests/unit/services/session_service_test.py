d197f7de1684facb27b6a5f1c476a1e3e7fe95c7
ChickyMasala <samuel.goh96@gmail.com>
2024-07-29 14:01:04 +0800

    add mock get

--- tests/unit/services/session_service_test.py
+++ tests/unit/services/session_service_test.py
@@ -5,7 +5,36 @@ from api.services.session_service import SessionService
 
 session_service: SessionService = init()["session_service"] 
 def test_session_service():
-    mock_get_item_return = {}
+    mock_get_item_return = {"session": {
+        "Item": {
+            "doc_html": {
+                "S": "<p>this is a test doc</p>"
+            },
+            "id": {
+                "S": "test"
+            },
+            "messages": {
+                "SS": [
+                    "{'message': 'Hi', 'isUser': false}",
+                    "{'message': 'Yo', 'isUser': true}"
+                ]
+            }
+        },
+        "ResponseMetadata": {
+            "HTTPHeaders": {
+                "connection": "keep-alive",
+                "content-length": "166",
+                "content-type": "application/x-amz-json-1.0",
+                "date": "Fri, 26 Jul 2024 10:22:24 GMT",
+                "server": "Server",
+                "x-amz-crc32": "4238721578",
+                "x-amzn-requestid": "M48B6NKO16JFTSJ4A51RKQ0UDVVV4KQNSO5AEMVJF66Q9ASUAAJG"
+            },
+            "HTTPStatusCode": 200,
+            "RequestId": "M48B6NKO16JFTSJ4A51RKQ0UDVVV4KQNSO5AEMVJF66Q9ASUAAJG",
+            "RetryAttempts": 0
+        }
+    }}
     with patch("api.clients.aws.dynamodb.DynamoDBClient.get_item", return_value=mock_get_item_return):
         assert(session_service.get_session(session_id="test")) == mock_get_item_return
 
